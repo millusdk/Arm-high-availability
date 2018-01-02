331f72a2f84ff99952e1f592b2e275af6635ce93
Emil Refn <emr@netcompany.com>
2018-01-02 09:21:00 +0100

    Removed almost all configuration from CreateFailoverCluster.ps1 to determine where the error preventing deployment is

--- DSC/CreateFailoverCluster.ps1
+++ DSC/CreateFailoverCluster.ps1
@@ -93,260 +93,255 @@ configuration CreateFailoverCluster
             RebootNodeIfNeeded = $false
         }
 
-        xSqlCreateVirtualDisk CreateVirtualDisk
-        {
-            DriveSize = $NumberOfDisks
-            NumberOfColumns = $NumberOfDisks
-            BytesPerDisk = 1099511627776
-            OptimizationType = $WorkloadType
-            RebootVirtualMachine = $false
-        }
-
-        WindowsFeature FC
-        {
-            Name = "Failover-Clustering"
-            Ensure = "Present"
-        }
-
-		WindowsFeature FailoverClusterTools 
-        { 
-            Ensure = "Present" 
-            Name = "RSAT-Clustering-Mgmt"
-			DependsOn = "[WindowsFeature]FC"
-        } 
-
-        WindowsFeature FCPS
-        {
-            Name = "RSAT-Clustering-PowerShell"
-            Ensure = "Present"
-        }
-
-        WindowsFeature ADPS
-        {
-            Name = "RSAT-AD-PowerShell"
-            Ensure = "Present"
-        }
-
-        xWaitForADDomain DscForestWait 
-        { 
-            DomainName = $DomainName 
-            DomainUserCredential= $DomainCreds
-            RetryCount = $RetryCount 
-            RetryIntervalSec = $RetryIntervalSec 
-	        DependsOn = "[WindowsFeature]ADPS"
-        }
-        
-        xComputer DomainJoin
-        {
-            Name = $env:COMPUTERNAME
-            DomainName = $DomainName
-            Credential = $DomainCreds
-	        DependsOn = "[xWaitForADDomain]DscForestWait"
-        }
-
-        xFirewall DatabaseEngineFirewallRule
-        {
-            Direction = "Inbound"
-            Name = "SQL-Server-Database-Engine-TCP-In"
-            DisplayName = "SQL Server Database Engine (TCP-In)"
-            Description = "Inbound rule for SQL Server to allow TCP traffic for the Database Engine."
-            DisplayGroup = "SQL Server"
-            State = "Enabled"
-            Access = "Allow"
-            Protocol = "TCP"
-            LocalPort = $DatabaseEnginePort -as [String]
-            Ensure = "Present"
-        }
-
-        xFirewall DatabaseMirroringFirewallRule
-        {
-            Direction = "Inbound"
-            Name = "SQL-Server-Database-Mirroring-TCP-In"
-            DisplayName = "SQL Server Database Mirroring (TCP-In)"
-            Description = "Inbound rule for SQL Server to allow TCP traffic for the Database Mirroring."
-            DisplayGroup = "SQL Server"
-            State = "Enabled"
-            Access = "Allow"
-            Protocol = "TCP"
-            LocalPort = "5022"
-            Ensure = "Present"
-        }
-
-        xFirewall ListenerFirewallRule
-        {
-            Direction = "Inbound"
-            Name = "SQL-Server-Availability-Group-Listener-TCP-In"
-            DisplayName = "SQL Server Availability Group Listener (TCP-In)"
-            Description = "Inbound rule for SQL Server to allow TCP traffic for the Availability Group listener."
-            DisplayGroup = "SQL Server"
-            State = "Enabled"
-            Access = "Allow"
-            Protocol = "TCP"
-            LocalPort = "59999"
-            Ensure = "Present"
-        }
-
-        xSqlLogin AddDomainAdminAccountToSysadminServerRole
-        {
-            Name = $DomainCreds.UserName
-            LoginType = "WindowsUser"
-            ServerRoles = "sysadmin"
-            Enabled = $true
-            Credential = $Admincreds
-        }
-
-        xADUser CreateSqlServerServiceAccount
-        {
-            DomainAdministratorCredential = $DomainCreds
-            DomainName = $DomainName
-            UserName = $SQLServicecreds.UserName
-            Password = $SQLServicecreds
-            Ensure = "Present"
-            DependsOn = "[xSqlLogin]AddDomainAdminAccountToSysadminServerRole"
-        }
-
-        xSqlLogin AddSqlServerServiceAccountToSysadminServerRole
-        {
-            Name = $SQLCreds.UserName
-            LoginType = "WindowsUser"
-            ServerRoles = "sysadmin"
-            Enabled = $true
-            Credential = $Admincreds
-            DependsOn = "[xADUser]CreateSqlServerServiceAccount"
-        }
-        
-        xSqlTsqlEndpoint AddSqlServerEndpoint
-        {
-            InstanceName = "MSSQLSERVER"
-            PortNumber = $DatabaseEnginePort
-            SqlAdministratorCredential = $Admincreds
-            DependsOn = "[xSqlLogin]AddSqlServerServiceAccountToSysadminServerRole"
-        }
-
-        xSQLServerStorageSettings AddSQLServerStorageSettings
-        {
-            InstanceName = "MSSQLSERVER"
-            OptimizationType = $WorkloadType
-            DependsOn = "[xSqlTsqlEndpoint]AddSqlServerEndpoint"
-        }
-
-        LocalConfigurationManager 
-        {
-            RebootNodeIfNeeded = $true
-        }
-
-        xCluster FailoverCluster
-        {
-            Name = $ClusterName
-            DomainAdministratorCredential = $DomainCreds
-            Nodes = $Nodes
-        }
-
-        xWaitForFileShareWitness WaitForFSW
-        {
-            SharePath = $SharePath
-            DomainAdministratorCredential = $DomainCreds
-
-        }
-
-        xClusterQuorum FailoverClusterQuorum
-        {
-            Name = $ClusterName
-            SharePath = $SharePath
-            DomainAdministratorCredential = $DomainCreds
-        }
-
-        xSqlServer ConfigureSqlServerWithAlwaysOn
-        {
-            InstanceName = $env:COMPUTERNAME
-            SqlAdministratorCredential = $Admincreds
-            ServiceCredential = $SQLCreds
-            Hadr = "Enabled"
-            MaxDegreeOfParallelism = 1
-            FilePath = "F:\DATA"
-            LogPath = "F:\LOG"
-            DomainAdministratorCredential = $DomainFQDNCreds
-            DependsOn = "[xCluster]FailoverCluster"
-        }
-
-        xSQLAddListenerIPToDNS AddLoadBalancer
-        {
-            LBName = $LBName
-            Credential = $DomainCreds
-            LBAddress = $LBAddress
-            DNSServerName = $DNSServerName
-            DomainName = $DomainName
-            DependsOn = "[xSqlServer]ConfigureSqlServerWithAlwaysOn"
-        }
-
-        xSqlEndpoint SqlAlwaysOnEndpoint
-        {
-            InstanceName = $env:COMPUTERNAME
-            Name = $SqlAlwaysOnEndpointName
-            PortNumber = 5022
-            AllowedUser = $SQLServiceCreds.UserName
-            SqlAdministratorCredential = $SQLCreds
-            DependsOn = "[xSqlServer]ConfigureSqlServerWithAlwaysOn"
-        }
-
-        xSqlServer ConfigureSqlServerSecondaryWithAlwaysOn
-        {
-            InstanceName = $SecondaryReplica
-            SqlAdministratorCredential = $Admincreds
-            Hadr = "Enabled"
-            DomainAdministratorCredential = $DomainFQDNCreds
-            DependsOn = "[xCluster]FailoverCluster"
-        }
-
-        xSqlEndpoint SqlSecondaryAlwaysOnEndpoint
-        {
-            InstanceName = $SecondaryReplica
-            Name = $SqlAlwaysOnEndpointName
-            PortNumber = 5022
-            AllowedUser = $SQLServiceCreds.UserName
-            SqlAdministratorCredential = $SQLCreds
-	    DependsOn="[xSqlServer]ConfigureSqlServerSecondaryWithAlwaysOn"
-        }
-        
-        xSqlAvailabilityGroup SqlAG
-        {
-            Name = $SqlAlwaysOnAvailabilityGroupName
-            ClusterName = $ClusterName
-            InstanceName = $env:COMPUTERNAME
-            PortNumber = 5022
-            DomainCredential =$DomainCreds
-            SqlAdministratorCredential = $Admincreds
-	        DependsOn="[xSqlEndpoint]SqlSecondaryAlwaysOnEndpoint"
-        }
-           
-        xSqlNewAGDatabase SQLAGDatabases
-        {
-            SqlAlwaysOnAvailabilityGroupName = $SqlAlwaysOnAvailabilityGroupName
-            DatabaseNames = $DatabaseNames
-            PrimaryReplica = $PrimaryReplica
-            SecondaryReplica = $SecondaryReplica
-            SqlAdministratorCredential = $SQLCreds
-	        DependsOn = "[xSqlAvailabilityGroup]SqlAG"
-        }
-
-        xSqlAvailabilityGroupListener SqlAGListener
-        {
-            Name = $SqlAlwaysOnAvailabilityGroupListenerName
-            AvailabilityGroupName = $SqlAlwaysOnAvailabilityGroupName
-            DomainNameFqdn = $LBFQName
-            ListenerPortNumber = $SqlAlwaysOnAvailabilityGroupListenerPort
-            ListenerIPAddress = $LBAddress
-            ProbePortNumber = 59999
-            InstanceName = $env:COMPUTERNAME
-            DomainCredential = $DomainCreds
-            SqlAdministratorCredential = $Admincreds
-            DependsOn = "[xSqlNewAGDatabase]SQLAGDatabases"
-        }
-
-		xPendingReboot RebootAfterPromotion {
-            Name = "Reboot after SQL always on preperation"
-            DependsOn = "[xSqlAvailabilityGroupListener]SqlAGListener"
-        }
+        #xSqlCreateVirtualDisk CreateVirtualDisk
+        #{
+        #    DriveSize = $NumberOfDisks
+        #    NumberOfColumns = $NumberOfDisks
+        #    BytesPerDisk = 1099511627776
+        #    OptimizationType = $WorkloadType
+        #    RebootVirtualMachine = $false
+        #}
+		#
+        #WindowsFeature FC
+        #{
+        #    Name = "Failover-Clustering"
+        #    Ensure = "Present"
+        #}
+		#
+		#WindowsFeature FailoverClusterTools 
+        #{ 
+        #    Ensure = "Present" 
+        #    Name = "RSAT-Clustering-Mgmt"
+		#	DependsOn = "[WindowsFeature]FC"
+        #} 
+		#
+        #WindowsFeature FCPS
+        #{
+        #    Name = "RSAT-Clustering-PowerShell"
+        #    Ensure = "Present"
+        #}
+		#
+        #WindowsFeature ADPS
+        #{
+        #    Name = "RSAT-AD-PowerShell"
+        #    Ensure = "Present"
+        #}
+		#
+        #xWaitForADDomain DscForestWait 
+        #{ 
+        #    DomainName = $DomainName 
+        #    DomainUserCredential= $DomainCreds
+        #    RetryCount = $RetryCount 
+        #    RetryIntervalSec = $RetryIntervalSec 
+	    #    DependsOn = "[WindowsFeature]ADPS"
+        #}
+        #
+        #xComputer DomainJoin
+        #{
+        #    Name = $env:COMPUTERNAME
+        #    DomainName = $DomainName
+        #    Credential = $DomainCreds
+	    #    DependsOn = "[xWaitForADDomain]DscForestWait"
+        #}
+		#
+        #xFirewall DatabaseEngineFirewallRule
+        #{
+        #    Direction = "Inbound"
+        #    Name = "SQL-Server-Database-Engine-TCP-In"
+        #    DisplayName = "SQL Server Database Engine (TCP-In)"
+        #    Description = "Inbound rule for SQL Server to allow TCP traffic for the Database Engine."
+        #    DisplayGroup = "SQL Server"
+        #    State = "Enabled"
+        #    Access = "Allow"
+        #    Protocol = "TCP"
+        #    LocalPort = $DatabaseEnginePort -as [String]
+        #    Ensure = "Present"
+        #}
+		#
+        #xFirewall DatabaseMirroringFirewallRule
+        #{
+        #    Direction = "Inbound"
+        #    Name = "SQL-Server-Database-Mirroring-TCP-In"
+        #    DisplayName = "SQL Server Database Mirroring (TCP-In)"
+        #    Description = "Inbound rule for SQL Server to allow TCP traffic for the Database Mirroring."
+        #    DisplayGroup = "SQL Server"
+        #    State = "Enabled"
+        #    Access = "Allow"
+        #    Protocol = "TCP"
+        #    LocalPort = "5022"
+        #    Ensure = "Present"
+        #}
+		#
+        #xFirewall ListenerFirewallRule
+        #{
+        #    Direction = "Inbound"
+        #    Name = "SQL-Server-Availability-Group-Listener-TCP-In"
+        #    DisplayName = "SQL Server Availability Group Listener (TCP-In)"
+        #    Description = "Inbound rule for SQL Server to allow TCP traffic for the Availability Group listener."
+        #    DisplayGroup = "SQL Server"
+        #    State = "Enabled"
+        #    Access = "Allow"
+        #    Protocol = "TCP"
+        #    LocalPort = "59999"
+        #    Ensure = "Present"
+        #}
+		#
+        #xSqlLogin AddDomainAdminAccountToSysadminServerRole
+        #{
+        #    Name = $DomainCreds.UserName
+        #    LoginType = "WindowsUser"
+        #    ServerRoles = "sysadmin"
+        #    Enabled = $true
+        #    Credential = $Admincreds
+        #}
+		#
+        #xADUser CreateSqlServerServiceAccount
+        #{
+        #    DomainAdministratorCredential = $DomainCreds
+        #    DomainName = $DomainName
+        #    UserName = $SQLServicecreds.UserName
+        #    Password = $SQLServicecreds
+        #    Ensure = "Present"
+        #    DependsOn = "[xSqlLogin]AddDomainAdminAccountToSysadminServerRole"
+        #}
+		#
+        #xSqlLogin AddSqlServerServiceAccountToSysadminServerRole
+        #{
+        #    Name = $SQLCreds.UserName
+        #    LoginType = "WindowsUser"
+        #    ServerRoles = "sysadmin"
+        #    Enabled = $true
+        #    Credential = $Admincreds
+        #    DependsOn = "[xADUser]CreateSqlServerServiceAccount"
+        #}
+        #
+        #xSqlTsqlEndpoint AddSqlServerEndpoint
+        #{
+        #    InstanceName = "MSSQLSERVER"
+        #    PortNumber = $DatabaseEnginePort
+        #    SqlAdministratorCredential = $Admincreds
+        #    DependsOn = "[xSqlLogin]AddSqlServerServiceAccountToSysadminServerRole"
+        #}
+		#
+        #xSQLServerStorageSettings AddSQLServerStorageSettings
+        #{
+        #    InstanceName = "MSSQLSERVER"
+        #    OptimizationType = $WorkloadType
+        #    DependsOn = "[xSqlTsqlEndpoint]AddSqlServerEndpoint"
+        #}
+		#
+        #xCluster FailoverCluster
+        #{
+        #    Name = $ClusterName
+        #    DomainAdministratorCredential = $DomainCreds
+        #    Nodes = $Nodes
+        #}
+		#
+        #xWaitForFileShareWitness WaitForFSW
+        #{
+        #    SharePath = $SharePath
+        #    DomainAdministratorCredential = $DomainCreds
+		#
+        #}
+		#
+        #xClusterQuorum FailoverClusterQuorum
+        #{
+        #    Name = $ClusterName
+        #    SharePath = $SharePath
+        #    DomainAdministratorCredential = $DomainCreds
+        #}
+		#
+        #xSqlServer ConfigureSqlServerWithAlwaysOn
+        #{
+        #    InstanceName = $env:COMPUTERNAME
+        #    SqlAdministratorCredential = $Admincreds
+        #    ServiceCredential = $SQLCreds
+        #    Hadr = "Enabled"
+        #    MaxDegreeOfParallelism = 1
+        #    FilePath = "F:\DATA"
+        #    LogPath = "F:\LOG"
+        #    DomainAdministratorCredential = $DomainFQDNCreds
+        #    DependsOn = "[xCluster]FailoverCluster"
+        #}
+		#
+        #xSQLAddListenerIPToDNS AddLoadBalancer
+        #{
+        #    LBName = $LBName
+        #    Credential = $DomainCreds
+        #    LBAddress = $LBAddress
+        #    DNSServerName = $DNSServerName
+        #    DomainName = $DomainName
+        #    DependsOn = "[xSqlServer]ConfigureSqlServerWithAlwaysOn"
+        #}
+		#
+        #xSqlEndpoint SqlAlwaysOnEndpoint
+        #{
+        #    InstanceName = $env:COMPUTERNAME
+        #    Name = $SqlAlwaysOnEndpointName
+        #    PortNumber = 5022
+        #    AllowedUser = $SQLServiceCreds.UserName
+        #    SqlAdministratorCredential = $SQLCreds
+        #    DependsOn = "[xSqlServer]ConfigureSqlServerWithAlwaysOn"
+        #}
+		#
+        #xSqlServer ConfigureSqlServerSecondaryWithAlwaysOn
+        #{
+        #    InstanceName = $SecondaryReplica
+        #    SqlAdministratorCredential = $Admincreds
+        #    Hadr = "Enabled"
+        #    DomainAdministratorCredential = $DomainFQDNCreds
+        #    DependsOn = "[xCluster]FailoverCluster"
+        #}
+		#
+        #xSqlEndpoint SqlSecondaryAlwaysOnEndpoint
+        #{
+        #    InstanceName = $SecondaryReplica
+        #    Name = $SqlAlwaysOnEndpointName
+        #    PortNumber = 5022
+        #    AllowedUser = $SQLServiceCreds.UserName
+        #    SqlAdministratorCredential = $SQLCreds
+	    #DependsOn="[xSqlServer]ConfigureSqlServerSecondaryWithAlwaysOn"
+        #}
+        #
+        #xSqlAvailabilityGroup SqlAG
+        #{
+        #    Name = $SqlAlwaysOnAvailabilityGroupName
+        #    ClusterName = $ClusterName
+        #    InstanceName = $env:COMPUTERNAME
+        #    PortNumber = 5022
+        #    DomainCredential =$DomainCreds
+        #    SqlAdministratorCredential = $Admincreds
+	    #    DependsOn="[xSqlEndpoint]SqlSecondaryAlwaysOnEndpoint"
+        #}
+        #   
+        #xSqlNewAGDatabase SQLAGDatabases
+        #{
+        #    SqlAlwaysOnAvailabilityGroupName = $SqlAlwaysOnAvailabilityGroupName
+        #    DatabaseNames = $DatabaseNames
+        #    PrimaryReplica = $PrimaryReplica
+        #    SecondaryReplica = $SecondaryReplica
+        #    SqlAdministratorCredential = $SQLCreds
+	    #    DependsOn = "[xSqlAvailabilityGroup]SqlAG"
+        #}
+		#
+        #xSqlAvailabilityGroupListener SqlAGListener
+        #{
+        #    Name = $SqlAlwaysOnAvailabilityGroupListenerName
+        #    AvailabilityGroupName = $SqlAlwaysOnAvailabilityGroupName
+        #    DomainNameFqdn = $LBFQName
+        #    ListenerPortNumber = $SqlAlwaysOnAvailabilityGroupListenerPort
+        #    ListenerIPAddress = $LBAddress
+        #    ProbePortNumber = 59999
+        #    InstanceName = $env:COMPUTERNAME
+        #    DomainCredential = $DomainCreds
+        #    SqlAdministratorCredential = $Admincreds
+        #    DependsOn = "[xSqlNewAGDatabase]SQLAGDatabases"
+        #}
+		#
+		#xPendingReboot RebootAfterPromotion {
+        #    Name = "Reboot after SQL always on preperation"
+        #    DependsOn = "[xSqlAvailabilityGroupListener]SqlAGListener"
+        #}
     }
 
 }
